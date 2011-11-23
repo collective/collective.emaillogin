9aea7f1a7a958939cd390ded166810d4acfb4504
Maurits van Rees <maurits@vanrees.org>
2011-11-23 11:25:18 +0000

    Include the readme in the PyPI description.
    svn path=/collective.emaillogin/trunk/; revision=246627

--- setup.py
+++ setup.py
@@ -4,14 +4,19 @@ import os.path
 versionfile = os.path.join('collective', 'emaillogin', 'version.txt')
 version = open(versionfile).read().strip()
 
+readme = open('README.txt').read().strip()
+
 historyfile = os.path.join('collective', 'emaillogin', 'HISTORY.txt')
 history = open(historyfile).read().strip()
+
+long_description = readme + '\n\n\n' + history
 
 setup(name='collective.emaillogin',
       version=version,
       description="Allow logins with email address rather than login name.",
-      long_description=history,
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 3.2",
